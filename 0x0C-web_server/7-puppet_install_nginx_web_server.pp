2ac743f5d3214897c6349d9c3c70ff63bacd4bc9
queensk <queenskisivuli@gmail.com>
2023-01-19 05:47:48 +0300

    updated the sever

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,11 +3,32 @@ package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'aaaaa':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => '
+server {
+        listen 80 default_server;
+        server_name _;
+        root /var/www/html;
+        index index.html;
+
+        server_name _;
+        error_page 404 /custom_404.html;
+        location = /custom_404.html {
+        root /usr/share/nginx/html;
+        internal;
+        }
+        
+        location / {
+                try_files $uri $uri/ /index.html;
+        }
+
+        location /redirect_me/ {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        add_header Content-Length 193;
+        }
+}',
+  require => Package['nginx'],
 }
 
 file { '/var/www/html/index.html':
