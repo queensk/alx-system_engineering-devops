cc97de594c35dcfb80b1787ff9e5ec424edbc9eb
queensk <queenskisivuli@gmail.com>
2023-03-04 03:09:47 +0300

    fixed errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,9 +12,9 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "My-User-Agent"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    if response.status_code >= 300:
+        return 0
+    else:
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
-    else:
-        return 0
