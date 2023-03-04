bfd4fb4c4a70b6f19ed87adf8a437413b2e32012
queensk <queenskisivuli@gmail.com>
2023-03-04 04:50:47 +0300

    get word count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -18,16 +18,17 @@ def count_words(subreddit, word_list, after=None, counts={}):
         for word, count in sorted_counts:
             print(f"{word.lower()}: {count}")
         return
-    
+
     # Query the Reddit API
     headers = {"User-Agent": "Mozilla/5.0"}
     params = {"limit": 100, "after": after}
     url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
     response = requests.get(url, headers=headers, params=params)
     if response.status_code != 200:
-        print(f"Error {response.status_code}: Could not query subreddit {subreddit}")
+        print(
+            f"Error {response.status_code}: Could not query subreddit {subreddit}")
         return
-    
+
     # Parse the titles of the articles and count the keywords
     data = response.json()
     articles = data["data"]["children"]
@@ -39,7 +40,7 @@ def count_words(subreddit, word_list, after=None, counts={}):
                     counts[word.lower()] += 1
                 else:
                     counts[word.lower()] = 1
-    
+
     # Recursively call the function with the next page of articles
     next_after = data["data"]["after"]
     count_words(subreddit, word_list, next_after, counts)
