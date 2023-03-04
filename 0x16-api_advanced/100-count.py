103559b18207bbad5be62f8a5b10be1398da4ab9
queensk <queenskisivuli@gmail.com>
2023-03-04 04:50:27 +0300

    get word count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -4,46 +4,42 @@ Queries the Reddit API, parses the title of all hot articles, and prints a sorte
 """
 
 
-def count_words(subreddit, word_list, word_count={}, after=None):
+def count_words(subreddit, word_list, after=None, counts={}):
     """
     Count words
     """
     import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
-                            .format(subreddit),
-                            params={"after": after},
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code != 200:
-        return None
-
-    info = sub_info.json()
-
-    hot_l = [child.get("data").get("title")
-             for child in info
-             .get("data")
-             .get("children")]
-    if not hot_l:
-        return None
-
-    word_list = list(dict.fromkeys(word_list))
-
-    if word_count == {}:
-        word_count = {word: 0 for word in word_list}
-
-    for title in hot_l:
-        split_words = title.split(' ')
+    # Base case: no more articles to query
+    if after == "":
+        # Sort counts by descending value and then by ascending key
+        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+        # Print the counts in the desired format
+        for word, count in sorted_counts:
+            print(f"{word.lower()}: {count}")
+        return
+    
+    # Query the Reddit API
+    headers = {"User-Agent": "Mozilla/5.0"}
+    params = {"limit": 100, "after": after}
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code != 200:
+        print(f"Error {response.status_code}: Could not query subreddit {subreddit}")
+        return
+    
+    # Parse the titles of the articles and count the keywords
+    data = response.json()
+    articles = data["data"]["children"]
+    for article in articles:
+        title = article["data"]["title"]
         for word in word_list:
-            for s_word in split_words:
-                if s_word.lower() == word.lower():
-                    word_count[word] += 1
-
-    if not info.get("data").get("after"):
-        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
-        sorted_counts = sorted(word_count.items(),
-                               key=lambda kv: kv[1], reverse=True)
-        [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
-    else:
-        return count_words(subreddit, word_list, word_count,
-                           info.get("data").get("after"))
+            if word.lower() in title.lower().split():
+                if word.lower() in counts:
+                    counts[word.lower()] += 1
+                else:
+                    counts[word.lower()] = 1
+    
+    # Recursively call the function with the next page of articles
+    next_after = data["data"]["after"]
+    count_words(subreddit, word_list, next_after, counts)
